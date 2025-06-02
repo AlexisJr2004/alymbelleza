7b1a2ce0ee290578161d12baf3256c6927c8514a
AlexisJr2004 <duranalexis879@gemail.com>
2025-06-02 11:12:36 -0500

    Perfil de usuario

--- backend/gestion-roles-productos/src/middlewares/authMiddleware.js
+++ backend/gestion-roles-productos/src/middlewares/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
+  console.log('TOKEN RECIBIDO:', token); // <-- Agrega esto
   if (!token) return res.status(401).json({ error: 'Token requerido.' });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
