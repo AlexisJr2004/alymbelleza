5ea0014e8a3f0c8a3e6c0579545c398364b31323
AlexisJr2004 <duranalexis879@gemail.com>
2025-06-09 00:12:00 -0500

    Centro de carrito de compras y pagos

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const authRoutes = require("./gestion-roles-productos/src/routes/authRoutes");
 const productRoutes = require("./gestion-roles-productos/src/routes/productRoutes");
 const appointmentRoutes = require('./gestion-roles-productos/src/routes/appointmentRoutes');
-
+const cartRoutes = require('./gestion-roles-productos/src/routes/cartRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -28,6 +28,7 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use('/api/appointments', appointmentRoutes);
+app.use('/api/cart', cartRoutes);
 
 // 2. Conexión a MongoDB
 const DB_URI =
