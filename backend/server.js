4ed6b8b7a7eae1b56079b9a1d641ec7c29968335
AlexisJr2004 <duranalexis879@gemail.com>
2025-06-20 01:01:18 -0500

    Corrección de Testimonios

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const productRoutes = require("./gestion-roles-productos/src/routes/productRoute
 const appointmentRoutes = require('./gestion-roles-productos/src/routes/appointmentRoutes');
 const cartRoutes = require('./gestion-roles-productos/src/routes/cartRoutes');
 const Testimonial = require('./gestion-roles-productos/src/models/testimonial');
-const authRoutes = require('./gestion-roles-productos/src/routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -31,7 +30,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/cart', cartRoutes);
-app.use('/api/auth', authRoutes);
 
 // 2. Conexión a MongoDB
 const DB_URI =
