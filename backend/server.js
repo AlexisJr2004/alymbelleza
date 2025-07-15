bba42240be77d717aca41627f455bafecad496e4
Alexis Duran <duranalexis879@gmail.com>
2025-07-14 19:39:05 -0500

    Descripción de los cambios realizados

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/cart', cartRoutes);
+app.use("/api/gallery", galleryRoutes);
 
 // 2. Conexión a MongoDB
 const DB_URI =
