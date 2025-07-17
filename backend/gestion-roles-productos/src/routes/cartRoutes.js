d1d232329d6fd69fa8859b911fa093943b4ba8ca
Alexis Duran <duranalexis879@gmail.com>
2025-07-17 00:20:43 -0500

    Correcciones

--- backend/gestion-roles-productos/src/routes/cartRoutes.js
+++ backend/gestion-roles-productos/src/routes/cartRoutes.js
@@ -4,7 +4,10 @@ const cartController = require('../controllers/cartController');
 const { verifyToken } = require('../middlewares/authMiddleware');
 
 router.get('/', verifyToken, cartController.getCart);
-router.post('/add', verifyToken, cartController.addToCart);
+router.post('/add', (req, res, next) => {
+  console.log("LLEGA A /api/cart/add", req.body, req.headers.authorization);
+  next();
+}, verifyToken, cartController.addToCart);
 router.post('/update', verifyToken, cartController.updateQuantity);
 router.post('/remove', verifyToken, cartController.removeFromCart);
 router.post('/clear', verifyToken, cartController.clearCart);
